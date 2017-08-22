c9ccf13d0dac17b6d5df8cca3c87fa28b1aa908b
mattli4 <knight8719@gmail.com>
2017-08-22 12:13:47 +0800

    eighth. same as seventh. more practice. ;)

---
+++ src/Solution206/Solution.java
@@ -0,0 +1,25 @@
+package Solution206;
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+        ListNode p = head;
+        ListNode tail = new ListNode(p.val);
+        while (p.next != null){
+            ListNode tmp = new ListNode(p.next.val);
+            tmp.next = tail;
+            tail = tmp;
+            p = p.next;
+        }
+        return tail;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(0);
+        ListNode l2 = new ListNode(1);
+        ListNode l3 = new ListNode(2);
+        l1.next = l2;
+        l2.next = l3;
+        ListNode l = new Solution().reverseList(l1);
+    }
+}
