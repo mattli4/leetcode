182b1abfb464b0dea75459b92d82f1f4a5ce46b3
mattli4 <knight8719@gmail.com>
2017-08-24 14:52:57 +0800

    tenth. so happy! ;)

---
+++ src/Solution199/Solution.java
@@ -0,0 +1,45 @@
+package Solution199;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new LinkedList<Integer>();
+        if(root == null) return list;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        Queue<TreeNode> tmp = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if(node.left != null){
+                tmp.offer(node.left);
+            }
+            if(node.right != null){
+                tmp.offer(node.right);
+            }
+            if(queue.isEmpty()){
+                list.add(node.val);
+                while (!tmp.isEmpty()){
+                    queue.offer(tmp.poll());
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        n1.left = n2;
+        n5.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        List<Integer> list = new Solution().rightSideView(n1);
+        System.out.println(list);
+    }
+}
---
+++ src/Solution199/TreeNode.java
@@ -0,0 +1,8 @@
+package Solution199;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
