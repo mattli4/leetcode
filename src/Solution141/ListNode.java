22f16f0d5650ff498a43f3959d69891189539aa9
mattli4 <knight8719@gmail.com>
2017-09-02 20:28:21 +0800

    14th. wuwuwu...

---
+++ src/Solution141/ListNode.java
@@ -0,0 +1,7 @@
+package Solution141;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/Solution141/Solution.java
@@ -0,0 +1,38 @@
+package Solution141;
+
+/*
+* if(head==null) return false;
+    ListNode walker = head;
+    ListNode runner = head;
+    while(runner.next!=null && runner.next.next!=null) {
+        walker = walker.next;
+        runner = runner.next.next;
+        if(walker==runner) return true;
+    }
+    return false;
+* */
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        ListNode cur = head;
+        ListNode nxt = head;
+        while (nxt.next != null && nxt.next.next != null){
+            cur = cur.next;
+            nxt = nxt.next.next;
+            if(cur == nxt) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(0);
+        ListNode l2 = new ListNode(1);
+        ListNode l3 = new ListNode(2);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l2;
+        boolean l = new Solution().hasCycle(l1);
+        System.out.println(l);
+    }
+}
