ae5d61bdd98bd2ae8c689c981c06383837f9d043
mattli4 <knight8719@gmail.com>
2017-09-02 09:55:23 +0800

    thirteenth. first submit and AC. WoW~

---
+++ src/Solution83/ListNode.java
@@ -0,0 +1,7 @@
+package Solution83;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/Solution83/Solution.java
@@ -0,0 +1,20 @@
+package Solution83;
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return null;
+        if(head.next == null) return head;
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null){
+            if(pre.val == cur.val){
+                pre.next = cur.next;
+                cur = cur.next;
+            }else{
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
