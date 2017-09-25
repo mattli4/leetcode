3277e7a8125a6db72e78e69037243dfc34b7f32a
mattli4 <knight8719@gmail.com>
2017-09-25 16:49:55 +0800

    19th. maybe hashmap run faster. O.O'

---
+++ src/Solution19/Solution.java
@@ -0,0 +1,22 @@
+package Solution19;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) return null;
+        ArrayList<ListNode> list = new ArrayList<>();
+        int len = 0;
+        ListNode p = head;
+        while (p != null){
+            len++;
+            list.add(p);
+            p = p.next;
+        }
+        int idx = len - n;
+        if(idx < 0) return null;
+        if(idx == 0) return head.next;
+        list.get(idx - 1).next = list.get(idx).next;
+        return head;
+    }
+}
