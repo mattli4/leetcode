ad39ba2c2a2117a687c8e5425b0597ee1b3189da
mattli4 <knight8719@gmail.com>
2017-10-07 10:35:33 +0800

    21th. memory limit error when using stack. so stack was deprecated.

---
+++ src/Solution92/Solution.java
@@ -0,0 +1,36 @@
+package Solution92;
+
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode h = new ListNode(0);
+        h.next = head;
+        ListNode pre = h;
+        for (int i = 0; i < m - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode start = pre.next;
+        ListNode then = start.next;
+        for (int i = 0; i < n - m; i++) {
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+        return h.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+      //  l5.next = l6;
+        ListNode l = new Solution().reverseBetween(l1, 2, 4);
+    }
+}
